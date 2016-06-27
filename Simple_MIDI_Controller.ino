a779e84f3c9dfb703ac379412cb95f7c349fd9af
magiobus <aalbertobus@gmail.com>
2016-06-27 00:30:58 -0600

    3 pots working

---
+++ Simple_MIDI_Controller.ino
@@ -0,0 +1,39 @@
+int val1, val2, val3 = 0;
+int lastVal1, lastVal2, lastVal3 = 0;
+
+void setup(){
+  //Use the same speed as Hairless Midi software
+  //Enable Serial
+  Serial.begin(9600);
+}
+
+void loop(){
+  //Reading POT 1
+  val1 = analogRead(1)/8; // Divide by 8 to get range of 0-127 for midi
+  //if pot changed it's value, we send a MIDI Message
+  if (val1 != lastVal1){
+    MIDImessage(176,1,val1);
+  }
+  lastVal1 = val1; //update last value
+
+  //Reading POT 2
+  val1 = analogRead(2)/8; // Divide by 8 to get range of 0-127 for midi
+  //If pot changed it's value, we send a MIDI Message
+  if (val2 != lastVal2){
+    MIDImessage(176,2,val2);
+  }
+  lastVal2 = val2; //update last value
+
+
+  //here we add a short delay to help prevent slight fluctuations, knocks on the pots etc. 
+  //Adding this helped to prevent my pots from jumpin up or down a value when slightly touched or knocked.
+  delay(10);
+}
+
+//function to send MIDI MESSAGE
+void MIDImessage(byte command, byte data1, byte data2) 
+{
+   Serial.write(command); //cc ccomand
+   Serial.write(data1);  //cc number
+   Serial.write(data2); //value
+}
